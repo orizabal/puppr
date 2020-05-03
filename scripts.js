e4600986696ba56d2cdc75f80e28dcdbe730b8a4
Keaton Lees <keatslees@yahoo.com>
2020-05-02 17:51:15 -0700

    selection

--- scripts.js
+++ scripts.js
@@ -5,8 +5,9 @@ function add_slide(n) {
     show_slide(slide_i += n);
 }
 
+var i;
+
 function show_slide(n) {
-    var i;
     var x = document.getElementsByClassName("dog-pic");
     if (n > x.length) {
         slide_i = 1;
@@ -18,4 +19,16 @@ function show_slide(n) {
         x[i].style.display = "none";
     }
     x[slide_i - 1].style.display = "block";
+
+    console.log("test")
+}
+
+var dog_array = new Array();
+
+function add_to_profile() {
+    // var length = dog_array.length;
+
+    dog_array[0] = new Image();
+    dog_array[0].src = "images/dog-play.jpg";
+
 }
